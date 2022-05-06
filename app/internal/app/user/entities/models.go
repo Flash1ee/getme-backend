59a6f061f7c7ff5be9ae54d056cf23c4e444bdae
Dmitry Varin <55987935+flash1ee@users.noreply.github.com>
2022-05-07 01:03:02 +0300

    fix tags in user entities

--- app/internal/app/user/entities/models.go
+++ app/internal/app/user/entities/models.go
@@ -3,15 +3,15 @@ package entities
 import "time"
 
 type User struct {
-	ID           int64     `json:"id"`
-	TelegramID   int64     `json:"tg_id"`
-	FirstName    string    `json:"first_name"`
-	LastName     string    `json:"last_name"`
-	Nickname     string    `json:"nickname"`
-	About        string    `json:"about"`
-	Avatar       string    `json:"avatar"`
-	Email        string    `json:"email"`
-	IsSearchable bool      `json:"is_searchable"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID           int64     `db:"id"`
+	TelegramID   int64     `db:"tg_id"`
+	FirstName    string    `db:"first_name"`
+	LastName     string    `db:"last_name"`
+	Nickname     string    `db:"nickname"`
+	About        string    `db:"about"`
+	Avatar       string    `db:"avatar"`
+	Email        string    `db:"email"`
+	IsSearchable bool      `db:"is_searchable"`
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
 }
