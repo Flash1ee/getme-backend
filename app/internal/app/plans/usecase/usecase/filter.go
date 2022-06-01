6693fd1b84ce3c920d3fa4205f0e982f439017f3
TheCompiler <vet_v2002@mail,ru>
2022-06-01 14:52:24 +0300

    fix adding

--- app/internal/app/plans/usecase/usecase/filter.go
+++ app/internal/app/plans/usecase/usecase/filter.go
@@ -45,7 +45,9 @@ func filterPlansByTasks(plans []entities.PlanWithUserAndTask) []entities.PlanWit
 				Tasks: make([]entities2.Task, 0),
 			}
 		}
-		planTasks[val.Plan.ID].Tasks = append(planTasks[val.Plan.ID].Tasks, val.Task)
+		if val.Task.ID.Valid {
+			planTasks[val.Plan.ID].Tasks = append(planTasks[val.Plan.ID].Tasks, val.Task)
+		}
 	}
 
 	var res []entities.PlanWithMentorAndTasks
